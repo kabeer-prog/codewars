c3a7e2aa665357c15156f397335206c5f815f222
kabeer-prog <kabeerlap@gmail.com>
2022-05-22 14:00:00 +0100

    Codewars

---
+++ 060/script.js
@@ -0,0 +1,7 @@
+// Convert a number to string
+function numberToString(num) {
+    // Return a string of the number here!
+    var num ;
+  var myString = num.toString(); //converts number to string "120"
+    return myString;
+  }
